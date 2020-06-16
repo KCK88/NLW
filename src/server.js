0b72b5e01c172b6104b70ce379abd5732a072b03
Claudio <claudiomeirabs@gmail.comm>
2020-06-16 18:44:05 -0300

    Add server script

---
+++ src/server.js
@@ -0,0 +1,90 @@
+const express = require("express")
+const server = express()
+
+//Pegar banco de dados
+const db = require("./database/db.js")
+
+
+//config pastas style
+server.use(express.static("public"))
+
+//habilitar o body na app
+
+server.use(express.urlencoded({ extended: true }))
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+//config server
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    //req.query:query string a nossa url
+    //console.log(req.query)
+    return res.render("create-point.html")
+})
+
+server.post("/savepoint", (req, res) => {
+
+    //console.log(req.body)
+    //inserir dados no db
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+
+        ) VALUES(?,?,?,?,?,?,?);
+    `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            return console.log(err)
+        }
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+        return res.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
+})
+
+server.get("/search-results", (req, res) => {
+
+    const search = req.query.search
+    if (search == "") { //pesquisa vazia
+        return res.render("search-results.html", { total: 0 })
+    }
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+        const total = rows.length
+        return res.render("search-results.html", { places: rows, total: total })
+    })
+})
+
+// ligar o server
+server.listen(3000)
+
+//01:31:44
